de115ac7e8e88c411bf13eb3d20e47f40f5d162f
payalcsureja <payalcsureja@gmail.com>
2018-05-10 17:04:47 -0400

    sidebar fixed css

--- src/app/app.js
+++ src/app/app.js
@@ -5,11 +5,16 @@ class App extends Component {
   render() {
     return (
       <div className={styles.app}>
-        <div className="container-fluid">
-        <header className={`row ${styles['app-header']}`}>            
-          <h1 className={styles['app-title']}>Welcome to {process.env.REACT_APP_TITLE}!</h1>
-        </header>
-           
+        <div>
+          {/* <header className={`row ${styles['app-header']}`}>            
+            <h1 className={styles['app-title']}>Welcome to {process.env.REACT_APP_TITLE}!</h1>
+          </header> */}
+          <nav className={`navbar navbar-dark ${styles['app-side-navbar']} ${styles['app-navbar']} ${styles['app-navbar-expand-lg']} ${styles['app-fixed-top']}`}  >
+            sidebar content
+          </nav>
+          <div class="container-fluid">
+          site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>
+          </div>           
         </div>
       </div>
     );
