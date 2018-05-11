2f49dbbed38dc14b0c4e315deabae4684f019974
payalcsureja <payalcsureja@gmail.com>
2018-05-11 16:47:53 -0400

    code cleanup for new theme

--- src/app/app.js
+++ src/app/app.js
@@ -6,13 +6,10 @@ class App extends Component {
     return (
       <div className={styles.app}>
         <div>
-          {/* <header className={`row ${styles['app-header']}`}>            
-            <h1 className={styles['app-title']}>Welcome to {process.env.REACT_APP_TITLE}!</h1>
-          </header> */}
-          <nav className={`navbar navbar-dark ${styles['app-side-navbar']} ${styles['app-navbar']} ${styles['app-navbar-expand-lg']} ${styles['app-fixed-top']}`}  >
-            sidebar content
-          </nav>
-          <div class="container-fluid">
+          <header className={`${styles['app-header']}`}>            
+            <h1 className={styles['app-title']}>{process.env.REACT_APP_TITLE}</h1>
+          </header>
+          <div className="container-fluid">
           site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>site content<br/>
           </div>           
         </div>
