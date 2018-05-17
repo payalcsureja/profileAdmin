eba70a761cc8dcbcc594bc3a59f584c3e6d91cae
payalcsureja <payalcsureja@gmail.com>
2018-05-17 10:43:30 -0400

    theme sidebar css update

--- src/app/components/sidebar/sidebar.js
+++ src/app/components/sidebar/sidebar.js
@@ -29,7 +29,7 @@ class Sidebar extends React.Component{
     }
     render(){
         return (
-            <div className="sidebar" data-color="blue">
+            <div className="sidebar" data-color="default">
                 <div className="logo">
                 	<a href="/" className="simple-text logo-mini">
                         <div className="logo-img">
