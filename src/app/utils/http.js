0f7ec243a3c94411eb85c8be3b7ec10fcfee886e
Payal Sureja <payalcsureja@gmail.com>
2018-06-26 20:39:15 -0400

    rest url config value per env

--- src/app/utils/http.js
+++ src/app/utils/http.js
@@ -2,7 +2,8 @@ import { Rxios } from 'rxios';
 var http = new Rxios({
   // all regular axios request configuration options are valid here
   // check https://github.com/axios/axios#request-config
-  baseURL: 'https://my-node-rest.herokuapp.com/api/',
+  // baseURL: 'https://my-node-rest.herokuapp.com/api/'
+  baseURL: process.env.REACT_APP_REST_URL,
 });
 
 export default http;
